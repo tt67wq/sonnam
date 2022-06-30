96363149ff8be6a4f9d3bec97ddc01b2b2ce684a
wanqiang <wanqiang@shizhuang-inc.com>
2022-07-01 00:58:09 +0800

    feat:add bin resp

--- lib/macros/response.ex
+++ lib/macros/response.ex
@@ -18,3 +18,29 @@ defmodule Sonnam.Macros.Response do
     end
   end
 end
+
+defmodule Sonnam.Macros.BinResponse do
+  defmacro __using__(_opts) do
+    quote do
+      require Logger
+
+      def bin_succ(conn, data) when is_binary(data) do
+        Logger.debug(%{"data" => inspect(data)})
+
+        conn
+        |> Plug.Conn.put_resp_content_type("application/octet-stream")
+        |> send_resp(200, data)
+      end
+
+      def bin_err(conn, msg \\ "Internal server error", code \\ 400)
+
+      def bin_err(conn, msg, code) do
+        Logger.error(%{"msg" => msg, "code" => code})
+
+        conn
+        |> Plug.Conn.put_resp_content_type("application/octet-stream")
+        |> Plug.Conn.send_resp(code, msg)
+      end
+    end
+  end
+end
